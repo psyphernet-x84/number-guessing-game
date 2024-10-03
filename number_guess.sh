905b7fb01782dd830c886ad0a466641750641b98
psyphernet-x84 machine <okehiloe@gmail.com>
2024-10-03 14:32:43 +0000

    feat: get user credentials

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# promp player for username
+echo -e "\nEnter your username:"
+read USERNAME
+
+# get username data
+USERNAME_RESULT=$($PSQL "SELECT username FROM players WHERE username='$USERNAME'")
+# get user id
+USER_ID_RESULT=$($PSQL "SELECT user_id FROM players WHERE username='$USERNAME'")
+
